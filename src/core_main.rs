2bd16c5550640a0267748a4db3e7d58a10541b2a
DevTeksir12 <developer.teksir@gmail.com>
2026-09-06 05:29:39 +0700

    brand: app name = TeksirViewer

--- src/core_main.rs
+++ src/core_main.rs
@@ -33,6 +33,7 @@ pub fn core_main() -> Option<Vec<String>> {
         return None;
     }
     crate::load_custom_client();
+    *config::APP_NAME.write().unwrap() = "TeksirViewer".to_owned();
     #[cfg(windows)]
     if !crate::platform::windows::bootstrap() {
         // return None to terminate the process
