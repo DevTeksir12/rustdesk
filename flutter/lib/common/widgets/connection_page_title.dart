819e861ffb0e13314b1397532d98d3f40920b77d
DevTeksir12 <developer.teksir@gmail.com>
2026-09-06 05:28:50 +0700

    brand: Teksir Remote Desktop

--- flutter/lib/common/widgets/connection_page_title.dart
+++ flutter/lib/common/widgets/connection_page_title.dart
@@ -11,7 +11,7 @@ Widget getConnectionPageTitle(BuildContext context, bool isWeb) {
           child: Row(
         children: [
           AutoSizeText(
-            translate('Control Remote Desktop'),
+            translate('Teksir Remote Desktop'),
             maxLines: 1,
             style: Theme.of(context)
                 .textTheme
@@ -36,3 +36,4 @@ Widget getConnectionPageTitle(BuildContext context, bool isWeb) {
     ],
   );
 }
+
